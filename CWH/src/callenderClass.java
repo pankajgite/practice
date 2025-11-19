a8a4b2a24e6a3a9123c8640a7c66dea6b16d5483
pankajgite <pankaj.gite2021@gmail.com>
2025-11-19 16:23:49 +0530

    hset

---
+++ CWH/src/callenderClass.java
@@ -0,0 +1,5 @@
+public class callenderClass {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ CWH/src/hset.java
@@ -0,0 +1,14 @@
+
+import java.util.HashSet;
+
+public class hset {
+    public static void main(String[] args) {
+        HashSet<Integer> h1 = new HashSet<>();
+        h1.add(1);
+        h1.add(4);
+        h1.add(3);
+        h1.add(5);
+        h1.add(3);
+        System.out.println(h1);
+    }
+}
