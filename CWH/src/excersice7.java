bfbd0d14c71e795ca642b2aa436918f0df2437e1
pankajgite <pankaj.gite2021@gmail.com>
2025-11-22 10:43:11 +0530

    java doc

---
+++ CWH/src/excersice7.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class excersice7 {
+    public static void main(String[] args) {
+        /*Create a library management System which is capable of issueing books to the students
+        Book should have info like :
+        1) Book name 
+        2) Book Author
+        3) Issued to 
+        4) Issued on 
+        user should be able to add books, issue books , return issues book
+        assume that all the users are been registred in the centeral library 
+        */
+
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
