07143da670e072d432602b3d22365bf00f8fe483
pankajgite <pankaj.gite2021@gmail.com>
2025-12-29 00:11:55 +0530

    Solution where positive and new are not equal

--- CWH/src/RearangeBySign.java
+++ CWH/src/RearangeBySign.java
@@ -1,28 +1,51 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RearangeBySign {
     public static  int[] rearrangeArray(int[] nums) {
+        ArrayList<Integer> pos= new ArrayList<>();
+        ArrayList<Integer> neg= new ArrayList<>();
         
-        int[] new1= new int[nums.length];
+       
         int posc=0;
-        int negc=1;
+        int negc=0;
+        int[] new1 = new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<0){
-                new1[negc]=nums[i];
-                negc=negc+2;
+            if(nums[i]>=0){
+                pos.add(nums[i]);
+                posc++;
             }else{
-                new1[posc]=nums[i];
-                posc=posc+2;
-            }
+                neg.add(nums[i]);
+                negc++;
 
+            }
         }
-        
+        int p=0;
+        int n=0;
+        for(int i=0;i<Math.min(posc, negc)*2;i++){
+            if(i%2==1){
+                new1[i]=neg.get(n);
+                n++;
+            }else{
+                new1[i]=pos.get(p);
+                p++;
+            }
+        }
+        for(int i =Math.min(posc, negc)*2;i<nums.length;i++){
+            if(posc>negc){
+                new1[i]=pos.get(p);
+                p++;
+            }else{
+                 new1[i]=neg.get(n);
+                n++;
+            }
+        }
         return new1;
     }
 
     
     public static void main(String[] args) {
-        int[] nums={3,1,-2,-5,2,-4};
+        int[] nums={-1,2,3,4,-3,1};
         System.out.print(Arrays.toString(rearrangeArray(nums)));
     }
 }
