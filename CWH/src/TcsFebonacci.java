27745d43c2d881770897b71433c13e8cdda8028f
pankajgite <pankaj.gite2021@gmail.com>
2026-03-11 01:01:03 +0530

    febo

--- CWH/src/TcsFebonacci.java
+++ CWH/src/TcsFebonacci.java
@@ -1,3 +1,47 @@
+
+import java.util.Scanner;
+
 public class TcsFebonacci {
-    main
+    public static void printFeboNnumbers(int n){
+        if(n<0){
+            System.out.println("Wrong Input!!");
+            return;
+        }
+        int a=0 , b=1;
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+
+    public static int printNthNumber(int n){
+        if(n<0){
+            System.out.println("Wrong Input!!");
+            return -1;
+        }
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        int a=0,b=1;
+        for(int i =2;i<=n;i++){
+            int c =a+b;
+            a=b;
+            b=c;
+
+        }
+        return b;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a numeber");
+        int n = sc.nextInt();
+        printFeboNnumbers(n);
+        System.out.println();
+        System.out.println("Nth term: "+printNthNumber(n));
+
+    }
 }
