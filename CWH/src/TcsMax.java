809fc73dd0c6e3999d04dc60e1fdf25bc73574e0
pankajgite <pankaj.gite2021@gmail.com>
2026-03-14 09:58:40 +0530

    Max of N numbers

--- CWH/src/TcsMax.java
+++ CWH/src/TcsMax.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 public class TcsMax {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.println();
+        System.out.println("Enter the Number of Elements:");
+        int n = sc.nextInt();
+        int max = -1020;
+        for(int i=0;i<n;i++){
+            System.out.println("Enter Element: ");
+            int x = sc.nextInt();
+            max = Math.max(max, x);
+        }
+
+        System.out.println("Maximum Element is: "+max);
     }
 }
